96a5b44814b4d1ecfbd46d35fe6a438838835597
Wilman Junior Vasquez Valdez <97067572+kwjvasquez@users.noreply.github.com>
2022-02-15 14:36:40 -0500

    Feature/add delete users (#2)
    * Add feature to user initialize
    
    * Add feature to create users.
    
    * Add feature to count users.
    
    * Add feature to find users for id
    
    * Add feature to find users for id (Fixed)
    
    * Add feature to find users for conditions
    
    * Add feature to update users
    
    * Add feature to delete users.
    
    * Improve the output messages.
    
    * Address feedback
    
    * Address feedback: class constant

--- user_class/user.rb
+++ user_class/user.rb
@@ -1,2 +1,109 @@
 class User
+  VALID_ATR = %i(first_name last_name email age address)
+
+  attr_reader :all
+
+  def initialize
+    @all = []
+  end
+
+  def create(id:, first_name:, last_name:, email:, **optional_info)
+    info = {
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      email: email
+    }
+    if optional_info[:age]
+      info.merge!({age: optional_info[:age]})
+    end
+    if optional_info[:address]
+      info.merge!({address: optional_info[:address]})
+    end
+
+    @all << info
+  end
+
+  def count
+    @all.size
+  end
+
+  def find(id)
+    @all.find { |user| user if id == user[:id] }
+  end
+
+  def where(**filters)
+    elements = []
+
+    @all.each do |user|
+      check = true
+      filters.each do |atr, value|
+        check = false unless user[atr] == value
+      end
+      elements << user if check
+    end
+
+    elements
+  end
+
+  def update(id:, **info_update)
+    @all.map do |user|
+      if id == user[:id]
+        info_update.each do |atr, value_update|
+          user[atr] = value_update if VALID_ATR.include?(atr)
+        end
+      end
+    end
+  end
+
+  def destroy(id:)
+    @all.delete_if { |user| id == user[:id] }
+  end
 end
+
+users = User.new
+user1_info = {
+  id: '0001',
+  first_name: 'Axel',
+  last_name: 'Ryzen',
+  email: 'aryzen@example.com'
+}
+users.create(**user1_info)
+
+user2_info = {
+  id: '0004',
+  first_name: 'Liliana',
+  last_name: 'Intel',
+  email: 'lintel@example.com',
+  age: 32
+}
+users.create(**user2_info)
+
+user3_info = {
+  id: '0009',
+  first_name: 'Axel',
+  last_name: 'Riot',
+  email: 'ariot@example.com'
+}
+users.create(**user3_info)
+
+user4_info = {
+  id: '0020',
+  first_name: 'Hiena',
+  last_name: 'Intel',
+  email: 'hintel@example.com',
+  address: 'CR 4B # 7'
+}
+users.create(**user4_info)
+
+puts "Users: #{users.all}"
+puts "Total users: #{users.count}"
+puts "User searched: #{users.find('0004')}"
+puts "Users matched: #{users.where(first_name: 'Axel')}"
+puts "User current info: #{users.find('0020')}"
+users.update(id: '0020', first_name: 'Artemis', age: 30, oh: 'new attribute')
+puts "User update info: #{users.find('0020')}"
+puts "Total users: #{users.count}"
+users.destroy(id: '0020')
+puts "Total users: #{users.count}"
+puts "Users: #{users.all}"
