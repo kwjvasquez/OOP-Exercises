a72ec3c66be4a770d06baf1483ccdd4a7f3e85f6
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-03-10 14:38:36 -0500

    Address feedback

--- user_class/spec/user_spec.rb
+++ user_class/spec/user_spec.rb
@@ -3,58 +3,58 @@
 require_relative '../user'
 require 'factories'
 
-RSpec.shared_context "user info" do
+RSpec.describe User do
+  subject { described_class.new }
+
+  let(:info_user) { attributes_for(:user) }
   let(:user) { subject.create(**info_user) }
   let(:user_id) { info_user[:id] }
 
   before do
     user
   end
-end
 
-RSpec.describe User do
-  subject { described_class.new }
+  describe "#create" do
+    let(:info_new_user) { attributes_for(:user) }
 
-  let(:info_user) { attributes_for(:user) }
-
-  describe ".create" do
     context "when all arguments are passed" do
       context "when the user id does not exist" do
         it "adds a user" do
-          expect { subject.create(**info_user) }.to change(subject, :count).by(1)
+          expect { subject.create(**info_new_user) }.to change(subject, :count).by(1)
         end
       end
 
       context "when the user id already exists" do
         it "does not add a user" do
-          subject.create(**info_user)
-          info_user2 = attributes_for(:user)
-          info_user2[:id] = info_user[:id]
-          expect { subject.create(**info_user2) }.to change(subject, :count).by(0)
+          info_new_user[:id] = user_id
+          expect { subject.create(**info_new_user) }.to change(subject, :count).by(0)
         end
       end
     end
 
     context "when an optional argument is not passed" do
+      def user_without_optional_arg
+        info_new_user.delete(:age)
+        subject.create(**info_new_user)
+
+        subject.find(info_new_user[:id])
+      end
+
       it "adds a user without the attribute" do
-        info_user.delete(:age)
-        subject.create(**info_user)
-        save_user = subject.find(info_user[:id])
+        save_user = user_without_optional_arg
         expect(save_user[:age]).to be_nil
       end
     end
 
     context "when a required argument is not passed" do
       it "raises an error" do
-        info_user.delete(:id)
-        expect { subject.create(**info_user) }.to raise_error(ArgumentError)
+        info_new_user.delete(:id)
+        expect { subject.create(**info_new_user) }.to raise_error(ArgumentError)
       end
     end
   end
 
   describe "#find" do
-    include_context "user info"
-
     context "when the user exists" do
       it "returns the information" do
         expect(subject.find(user_id)).to eq(info_user)
@@ -70,13 +70,11 @@ RSpec.describe User do
     end
   end
 
-  describe ".update" do
-    include_context "user info"
-
+  describe "#update" do
     let(:update_info) { attributes_for(:user).except(:id, :last_name, :email) }
 
     context "When the user exists" do
-      it "update the information" do
+      it "updates the information" do
         expect { subject.update(id: user_id, **update_info) }.to change { subject.find(user_id) }
       end
 
